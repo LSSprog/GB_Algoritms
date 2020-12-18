b50a8e1f57026aaf9fa53636c23753b45a5c5397
LSS <fedexss@bk.ru>
2020-12-19 01:05:34 +0300

    домашнее задание к 7 уроку (Графы)

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,6 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("Algoritms");
 
-        /*Graph graph = new Graph(5);
-        graph.addEdge(1,2);
-        graph.addEdge(0,4);
-        graph.addEdge(1,4);
-        graph.addEdge(1,0);
-
-      System.out.println(graph.getAdjList(1));
-
-        DepthFirstPaths dfp = new DepthFirstPaths(graph, 2);
-        System.out.println(dfp.hasPathTo(0));
-        System.out.println(dfp.pathTo(0));
-
-
-        BreadthFirstPaths bfp = new BreadthFirstPaths(graph, 2);
-        System.out.println(bfp.hasPathTo(0));
-        System.out.println(bfp.pathTo(0));*/
 
         //task
         Graph graph = new Graph(10);
