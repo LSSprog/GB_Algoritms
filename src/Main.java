30a9412e88821061bac22e49d1e9bf0fa412dcba
LSS <fedexss@bk.ru>
2020-12-20 11:50:26 +0300

    домашнее задание к 8 уроку (Хэш таблицы)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Algoritms");
+        //System.out.println("Algoritms");
+
+        ChainHashMap<Integer, String> chm = new ChainHashMap<>();
+        chm.put(1, "A");
+        chm.put(3, "C");
+        chm.put(2, "B");
+        chm.put(5, "E");
+        chm.put(4, "D");
+        chm.put(3, "W");
+
+        System.out.println(chm);
+
+        //task
+        chm.delete(5);
+        System.out.println(chm);
     }
 }
